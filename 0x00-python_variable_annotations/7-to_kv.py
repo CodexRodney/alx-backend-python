5ffe9d16aa892f0ddfe6007b661f26f13628d79a
CodexRodney <rodney002mwanje@gmail.com>
2022-08-04 17:53:50 +0300

    Updated file

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ Defines to_kv
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Union[Tuple[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Union[Tuple[str, float]]:
     """
     Returns a tuple of k and square of v
     """
