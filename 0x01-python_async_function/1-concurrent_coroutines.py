00b9ac02f96547d6e79ecde37616a13fa0a4d871
CodexRodney <rodney002mwanje@gmail.com>
2022-08-23 16:52:36 +0300

    Fixed type annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[int]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Calls wait_random n times with  specified
     max_delay
