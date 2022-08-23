6e45262d0056a3bcd54c4b7a2c219c5a259aa5ae
CodexRodney <rodney002mwanje@gmail.com>
2022-08-23 12:15:17 +0300

    Added 0-basic_async_syntax

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Defines a function wait_random
+"""
+
+import random
+import asyncio
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Takes in an integer and generates a random value
+    between 0 and max-delay and delays for that amount of
+    time then returns the ransom number
+    """
+    to_wait = random.uniform(0, max_delay) # time to delay
+    await asyncio.sleep(to_wait)
+    return to_wait
