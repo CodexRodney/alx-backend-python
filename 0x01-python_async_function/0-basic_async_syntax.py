c9c3dec95f3851734ace881f36c3a5fa61e3590c
CodexRodney <rodney002mwanje@gmail.com>
2022-08-23 12:18:01 +0300

    Fixed Pycodestyle

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,12 +6,13 @@ Defines a function wait_random
 import random
 import asyncio
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
     Takes in an integer and generates a random value
     between 0 and max-delay and delays for that amount of
     time then returns the ransom number
     """
-    to_wait = random.uniform(0, max_delay) # time to delay
+    to_wait = random.uniform(0, max_delay)  # time to delay
     await asyncio.sleep(to_wait)
     return to_wait
