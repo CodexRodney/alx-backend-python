ccab03683a8489e13d7d525930702503a9848cbe
CodexRodney <rodney002mwanje@gmail.com>
2022-08-23 17:43:45 +0300

    Added 3-tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Defines a functon task_wait_random
+"""
+
+import asyncio
+from typing import Callable
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Callable:
+    """
+    Returns an asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))
