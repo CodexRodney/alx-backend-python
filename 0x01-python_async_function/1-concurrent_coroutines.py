1b623c6a715ab0b0e6f8cf42672fdd21d77e6faa
CodexRodney <rodney002mwanje@gmail.com>
2022-08-23 16:46:12 +0300

    Added 1-concurrent_coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Defines a function wait_n
+"""
+
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[int]:
+    """
+    Calls wait_random n times with  specified
+    max_delay
+    """
+    delays = []  # list of delays
+    for i in range(n):
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+    sorted_delays = sorted(delays)
+    return sorted_delays
